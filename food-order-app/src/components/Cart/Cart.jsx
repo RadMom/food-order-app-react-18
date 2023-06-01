b2c868d5f7fc932f4940fbcc340e53fa609587f7
RadMom <RadMom1111@gmail.com>
2023-06-01 11:42:39 +0300

    140. Managing Cart & Modal State

--- food-order-app/src/components/Cart/Cart.jsx
+++ food-order-app/src/components/Cart/Cart.jsx
@@ -13,14 +13,14 @@ const Cart = (props) => {
     );
 
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amaunt</span>
                 <span>31.55</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes["button--alt"]}>Close</button>
+                <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
