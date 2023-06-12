d6a9351fc5f70b5ccd818afa8d415c7331a1fe5f
RadMom <RadMom1111@gmail.com>
2023-06-12 14:25:27 +0300

    Error handler added

--- food-order-app/src/components/Meals/AvailableMeals.jsx
+++ food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -7,12 +7,18 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
 
     useEffect(() => {
         const fetchMeals = async () => {
             const response = await fetch(
                 "https://food-app-a856d-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
             );
+
+            if (!response.ok) {
+                throw new Error("Something went wrong"); //that will be our error.message property
+            }
+
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -29,13 +35,25 @@ const AvailableMeals = () => {
             setIsLoading(false);
         };
 
-        fetchMeals();
+        fetchMeals().catch((error) => {
+            console.log(error);
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
     }, []);
 
     if (isLoading) {
         return (
             <section className={classes.MealsLoading}>
                 <p>Loading...</p>
+            </section>
+        );
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
             </section>
         );
     }
